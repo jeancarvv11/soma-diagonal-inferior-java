def2954d344c9f102c1e95d35be077d8c94ebd2e
Jean Carlo <jeancarlo.carvalho@hotmail.com>
2026-05-25 19:49:13 -0300

    Update SomaDiagonalInferior.java

--- SomaDiagonalInferior.java
+++ SomaDiagonalInferior.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Ex9 {
+public class SomaDiagonalInferior {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int [][] m =  {
